2781a514eb85fca1fc0caf9b18a3f06034fbf1c5
Riley <riley.childs@yahoo.com>
2021-11-03 19:16:40 -0400

    Second Commit

--- include/footer.php
+++ include/footer.php
@@ -1,11 +1,8 @@
 <footer>
-
-<a href="index.php">Home </a>|
-<a href="mtsnow.php">Mt. Snow </a>|
-<a href="stratton.php">Stratton </a>|
-<a href="bromley.php">Bromley </a>|
-<a href="newsletterSignup.php">Newsletter</a>|
-<a href="quiz.php">Quiz </a>
-
-
+  <a href="index.php">Home </a>|
+  <a href="mtsnow.php">Mt. Snow </a>|
+  <a href="stratton.php">Stratton </a>|
+  <a href="bromley.php">Bromley </a>|
+  <a href="newsletterSignup.php">Newsletter</a>|
+  <a href="quiz.php">Quiz </a>
 </footer>

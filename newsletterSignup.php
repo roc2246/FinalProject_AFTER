3aa0d01354d290017177bb14d9ad3a7b133351fc
Riley <riley.childs@yahoo.com>
2021-11-24 17:34:46 -0500

    Added auto-email to subscribe form

--- newsletterSignup.php
+++ newsletterSignup.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f70f47bd9a4c25da1d22843be5f22451384be0e3a12b70a00fe2e6951e803084
-size 2669
+oid sha256:fcd3d42a863cbb9a3f55e5256efb21ddc26182d365dfa2a0f9d0f4f48ec59ba3
+size 3100

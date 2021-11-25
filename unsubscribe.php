0cd2eacd3269be8c98a4b843ec1dd4bf9ddd9b6c
Riley <riley.childs@yahoo.com>
2021-11-25 11:13:04 -0500

    Created code to send confirmation email on unsubscribe

--- unsubscribe.php
+++ unsubscribe.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:948eade2350deede633403de1a72f1e89cb2411a2e938b507720b595dbe96b23
-size 1163
+oid sha256:8d7d9488699c3d9a1dfd24e0d41274fe6fb950bf4a870ab91436df05cba4eb64
+size 1616
